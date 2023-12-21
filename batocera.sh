0d37d45fe93b0f42994d439ca0a7ff6ed36c90c3
Raspberry Pi Buenos Aires <38385411+Luciano2018@users.noreply.github.com>
2023-12-21 14:21:48 -0300

    Update batocera.sh

--- batocera.sh
+++ batocera.sh
@@ -10,7 +10,7 @@ echo "Getting Read and Write system"
 mount -o remount,rw /
 mount -o remount,rw /boot
 echo
-RUTA=https://raw.githubusercontent.com/Luciano2018/rom/master/
+RUTA=https://raw.githubusercontent.com/Luciano2018/NeoGeo/master/
 echo
 echo "Clean"
 echo
