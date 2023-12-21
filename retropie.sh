ab512c722e64166f67b507c92aeeaf3591768dee
Raspberry Pi Buenos Aires <38385411+PIBSAS@users.noreply.github.com>
2023-12-21 14:53:29 -0300

    Update retropie.sh

--- retropie.sh
+++ retropie.sh
@@ -6,7 +6,7 @@
 ###########################################################################
 cd
 echo
-RUTA=https://raw.githubusercontent.com/Luciano2018/NeoGeo/master/
+RUTA=https://raw.githubusercontent.com/PIBSAS/NeoGeo/master/
 echo
 echo "Clean"
 echo
