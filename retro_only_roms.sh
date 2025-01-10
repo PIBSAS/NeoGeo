2591d56bab74cc05443d477aea45f63814d98db4
Luciano's tech <38385411+PIBSAS@users.noreply.github.com>
2025-01-10 12:20:52 -0300

    Update retro_only_roms.sh

--- retro_only_roms.sh
+++ retro_only_roms.sh
@@ -6,7 +6,7 @@
 ###########################################################################
 cd
 echo
-RUTA=https://raw.githubusercontent.com/PIBSAS/NeoGeo/master/
+RUTA="https://raw.githubusercontent.com/PIBSAS/NeoGeo/master/"
 echo
 echo "Clean"
 echo
